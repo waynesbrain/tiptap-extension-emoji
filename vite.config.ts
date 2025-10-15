894d1cef941a85da7ce32885b50f5239e4f0db5b
waynesbrain <175370544+waynesbrain@users.noreply.github.com>
2025-10-15 00:16:48 -0400

    Fix vite minify issue.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { resolve } from "node:path";
+import { transform } from "esbuild";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
@@ -7,6 +8,7 @@ export default defineConfig({
   publicDir: false,
   build: {
     // assetsInlineLimit: 0, // Consider if CSS gets inline?
+    minify: "esbuild",
     lib: {
       entry: resolve(__dirname, "./src/index.ts"),
       name: "tiptap-extension-emoji",
@@ -40,10 +42,11 @@ export default defineConfig({
       //   },
       // },
     },
-    sourcemap: true,
+    // sourcemap: true,
     emptyOutDir: true,
   },
   plugins: [
+    minifyEs(),
     /**
      * Bundle type declarations into a single index.d.ts file.
      * See https://github.com/vitejs/vite/issues/2049
@@ -54,3 +57,22 @@ export default defineConfig({
     }),
   ],
 });
+
+// Workaround for https://github.com/vitejs/vite/issues/6555
+function minifyEs() {
+  return {
+    name: "minifyEs",
+    renderChunk: {
+      order: "post" as const,
+      async handler(code: any, _chunk: any, outputOptions: any) {
+        if (
+          outputOptions.format === "es"
+          // && (!bundleComponents || chunk.fileName.endsWith(".min.js"))
+        ) {
+          return await transform(code, { minify: true });
+        }
+        return code;
+      },
+    },
+  };
+}
